737cbab60a4f0305e9f3ab3c80b57d7a820aa4af
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-30 08:37:41 -0300

    test: improve tests

--- src/exercises/triangles/_test/index.test.js
+++ src/exercises/triangles/_test/index.test.js
@@ -1,16 +1,11 @@
 const testTriagles = require('../index.js')
 
-// Quando verdadeiro o teste passa
-// Quando falso retorna o erro
-function assert(value) {
-  if(!value) throw new Error("Invalid value")
+function assert(value, description) {
+  if(!value) throw new Error(`Teste failed: ${description}`)
 }
 
-console.log('testTriangles shuld be a function', typeof(testTriagles) === 'function')
-assert(testTriagles)
+console.log('Test passed: Verify if testTriagles is a function ...')
+assert(typeof(testTriagles === "functions"), "testTriagles should be a function")
 
-console.log('TestTriagles should be a triangle');
-assert(testTriagles(3, 5, 1))
-// Você pode testar se existe uma função nessa linha de ser um prototype
-// ou pode ir construindo seu programa e ir testando
-// lembre-se que o teste vem primeiro
+console.log("Teste passed: Verify if testTriagles is a triangle ...")
+assert(testTriagles(3, 5, 1), "testTriangles should be a triangle")
