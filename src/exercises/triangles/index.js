2eb1970c8abbf7948a83d4bfcc65661667d16f6d
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-31 09:22:59 -0300

    feat: implement a program

--- src/exercises/triangles/index.js
+++ src/exercises/triangles/index.js
@@ -1,4 +1,18 @@
-const testTriagles = () => {
-}
+const classifyTriangles = (a, b, c) => {
+    if (a + b > c && b + c > a && a + c > b) {
+        switch (true) {
+            case a === b && b === c:
+                return "Equilateral Triangle";
+            case a === b || b === c || c === a:
+                return "Isosceles Triangle";
+            default:
+                return "Scalene Triangle";
+        }
+    }
+};
 
-module.exports = testTriagles
+console.log(classifyTriangles(3, 3, 3))
+console.log(classifyTriangles(3, 5, 3))
+console.log(classifyTriangles(2, 4, 3))
+
+module.exports = classifyTriangles;
