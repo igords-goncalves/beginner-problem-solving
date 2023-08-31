24e8615b9370c4eb8ab55a4e9584a6be51789513
Igor Gonçalves <igordossantos38@gmail.com>
2023-08-31 09:22:14 -0300

    test: implement units tests

--- src/exercises/triangles/_test/index.test.js
+++ src/exercises/triangles/_test/index.test.js
@@ -1,11 +1,20 @@
-const testTriagles = require('../index.js')
+const classifyTriangles = require('../index.js')
 
 function assert(value, description) {
   if(!value) throw new Error(`Teste failed: ${description}`)
 }
 
-console.log('Test passed: Verify if testTriagles is a function ...')
-assert(typeof(testTriagles === "functions"), "testTriagles should be a function")
+console.log('Test passed: Verify if classifyTriangles is a function ...')
+assert(typeof(classifyTriangles === "functions"), "classifyTriangles should be a function")
 
-console.log("Teste passed: Verify if testTriagles is a triangle ...")
-assert(testTriagles(3, 5, 1), "testTriangles should be a triangle")
+console.log("Test passed: Verify if classifyTriangles is a triangle ...")
+assert(classifyTriangles(3, 5, 3), "classifyTriangles should be a triangle")
+
+console.log("Test passed: Verify if classifyTriangles is a Equilateral Triangle ...")
+assert(classifyTriangles(3, 3, 3) === "Equilateral Triangle", "classifyTriangle should be a Equilateral Triangle")
+
+console.log("Test passed: Verify if classifyTriangles is a Isosceles Triangle ...")
+assert(classifyTriangles(3, 5, 3) === "Isosceles Triangle", "classifyTriangle should be a Isosceles Triangle")
+
+console.log("Test passed: Verify if classifyTriangles is a Scalene Triangle ...")
+assert(classifyTriangles(4, 2, 3) === "Scalene Triangle", "classifyTriangle should be a Scalene Triangle")
