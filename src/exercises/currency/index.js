c9b65ab44b91a705386028d270ca9ce0227ccd11
Igor Gonçalves <igordossantos38@gmail.com>
2023-09-01 23:45:59 -0300

    feat: create a function that forma currency

---
+++ src/exercises/currency/index.js
@@ -0,0 +1,7 @@
+function formatCurrency (value) {
+    const currency = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(value )
+    return currency
+}
+console.log(formatCurrency(34.6))
+
+module.exports = formatCurrency;
